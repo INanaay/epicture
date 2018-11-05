f6a6cb8513cfeeb4acd91c4c53c519ce95d2f660
Lebon Nathan <nathan.lebon@epitech.eu>
2018-11-05 16:49:43 +0100

    Reworked repo

--- epicture/android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++
@@ -1,8 +0,0 @@
-package host.exp.exponent.generated;
-
-// This file is auto-generated. Please don't rename!
-public class DetachBuildConstants {
-
-  public static final String DEVELOPMENT_URL = "expfb4c7e21baf046c091dde37f4b7b2c61://10.41.173.216:19000";
-
-}
